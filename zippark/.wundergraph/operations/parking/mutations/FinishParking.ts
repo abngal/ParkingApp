63f11cf7eb894cbf8ca4692d487f6bb949665660
abner <abgalenojr@gmail.com>
2023-05-23 22:44:29 +0000

    adds dunergraph typescript operations

---
+++ zippark/.wundergraph/operations/parking/mutations/FinishParking.ts
@@ -0,0 +1,93 @@
+import { Temporal } from '@js-temporal/polyfill';
+
+import { PrismaClient } from '@prisma/client'
+import { InternalError } from '@wundergraph/sdk/dist/client/errors';
+
+import { createOperation, z } from '../../../generated/wundergraph.factory';
+
+const prisma = new PrismaClient()
+
+
+function timeDiffToNow(dateTimeStart: string, temporalNow: Temporal.PlainDateTime) {
+	/**
+	 * rounds to nearest integer
+	 *  - ex: 5.4 => 5, 5.5 => 6
+	 */
+
+	const datetimeStart = Temporal.PlainDateTime.from(dateTimeStart.replace('Z',''));
+
+	const diff = temporalNow.since(datetimeStart);
+	const diffWithDecimalMinutes = diff.hours + ( diff.minutes / 60);
+	return diffWithDecimalMinutes;
+}
+
+export default createOperation.mutation({
+	input: z.object({
+		parkingTransactionId: z.string(),
+	}),
+	handler: async ({ input, operations }) => {
+		console.log("input", input);
+		// console.log("operations", operations);
+		/**
+		 * Compute total parking amount owed
+		 * 	- total = fixed amount + ( hours * variable amount) 
+		 */
+
+		const txn = await prisma.parking_transactions.findFirst( { 
+			select: { 
+				id: true,
+				vehicle_plate: true,
+				datetime_in: true,
+				datetime_out: true,
+				amount: true,
+				parking_rates: { 
+					include: { vehicle_types: true}  
+				}
+			},  
+			where: { 	
+				id : { equals: parseInt(input.parkingTransactionId) }
+			}
+		});
+		
+		if ( !txn ) {
+			throw new InternalError({ message: 'Missing <Parking Transaction> object'});
+		} else if ( !txn.parking_rates?.min_amount || !txn.parking_rates?.variable_amount || !txn.parking_rates?.min_hours) {
+			throw new InternalError({ message: 'Missing required properties on <Parking Transaction>'});
+		}
+
+		const temporalNow = Temporal.Now.plainDateTimeISO();
+		const timeDiffInHours: number = timeDiffToNow(txn.datetime_in.toISOString(), temporalNow);
+
+		const excessOverMinHours = Math.abs( timeDiffInHours - txn.parking_rates?.min_hours ); 
+		const hasVariableHours = ( excessOverMinHours > 0 );
+		let totalAmount = 0;
+		// (a) just the total fixed amount
+		let fixedAmount = txn.parking_rates?.min_amount; 		 // (a) base/minimum amount
+		totalAmount += fixedAmount;
+		// (b) or add variable hours
+		if (hasVariableHours) {
+			const variableAmount = excessOverMinHours * txn.parking_rates?.variable_amount 
+			console.log("variableAmount", variableAmount);
+			totalAmount += variableAmount;
+		}
+
+		const updateParams = {
+			where: { 
+				id: txn.id, 
+			}, 
+			data: {
+				datetime_out: new Date(Temporal.Now.plainDateTimeISO().toString()),
+				amount: Math.ceil(totalAmount),
+			}
+		};
+		const updated = await prisma.parking_transactions.update(updateParams);
+
+		const response = {
+			id: updated.id.toString(),			
+			amount: updated.amount,			
+			datetime_in: updated.datetime_in,
+			datetime_out: updated.datetime_out,
+		}
+		return response;
+	},
+});
---
+++ zippark/.wundergraph/operations/parking/mutations/StartParking.ts
@@ -0,0 +1,105 @@
+import { Temporal, Intl, toTemporalInstant } from '@js-temporal/polyfill';
+import crypto from "crypto"
+
+import { PrismaClient } from '@prisma/client'
+import { InternalError } from '@wundergraph/sdk/dist/client/errors';
+const prisma = new PrismaClient()
+
+import { createOperation, z } from '../../../generated/wundergraph.factory';
+
+
+function timeDiffToNow(dateTimeStart: string) {
+	/**
+	 * rounds to nearest integer
+	 *  - ex: 5.4 => 5, 5.5 => 6
+	 */
+	const datetimeStart = Temporal.PlainDateTime.from(dateTimeStart.replace('Z',''));
+	const datetimeNow = Temporal.Now.plainDateTimeISO();
+
+	const diff = datetimeNow.since(datetimeStart);
+	const diffWithDecimalMinutes = diff.hours + ( diff.minutes / 40);
+
+	return diffWithDecimalMinutes;
+}
+
+function random3Letters(): string {
+	/*
+		output samples: 'ABC', 'GTX', 'ZCX'
+	*/
+	return crypto.randomUUID().replace(/[0-9-]/g, '').substring(0,3).toUpperCase()
+}
+
+
+export default createOperation.mutation({
+	input: z.object({
+		vehicleCode: z.string(),
+		entranceCode: z.string(),
+	}),
+	handler: async ({ input, operations }) => {
+		/**
+		 * Inserts a new parking transaction
+		 *  - (a) gets the id of the active parking rate first before insert of record
+		 * 	- (b) write the new parking transaction
+		 * 		- start time is current time
+		 */
+
+		// (a)
+		const parkingRate = await prisma.parking_rates.findFirst( { 
+			select: { 
+				id: true,
+				is_active: true
+			},  
+			where: { 	
+				is_active: true, 
+				vehicle_types: { 
+					is: { code: input.vehicleCode } 
+				} 
+			} 
+		});
+		
+		const entrance = await prisma.entrances.findFirst( { 
+			select: { 
+				id: true,
+				code: true,
+				name: true,
+			},  
+			where: { 	
+				code: input.entranceCode,
+			} 
+		});
+
+		if (!entrance || !entrance?.id) {
+			throw new InternalError({ message: 'unexpected entrance code'});
+		}
+
+		if (!parkingRate) {
+			throw new InternalError({ message: 'undefined parkingRate'});
+		}
+
+		// (b)
+		const data = { 
+			vehicle_plate: random3Letters() + ' 222',
+			amount: undefined, // to update on finish of parking
+			parking_rate: parkingRate.id,
+			entrance: entrance?.id,
+		}
+
+		const newParkingTxn = await prisma.parking_transactions.create({ data: data });
+		if (!newParkingTxn) {
+			throw new InternalError({ message: 'failed to create on parking_transacion'});
+		}
+
+		const response = {
+			id: newParkingTxn.id,
+			created_at: newParkingTxn.created_at,
+			vehicle_plate: newParkingTxn.vehicle_plate,
+			datetime_in: newParkingTxn.datetime_in,
+			datetime_out: newParkingTxn.datetime_out,
+			amount: newParkingTxn.amount,
+			parkingRate: newParkingTxn.parking_rate,
+			entrance: newParkingTxn.entrance,
+		}
+
+		return response;
+	},
+});
